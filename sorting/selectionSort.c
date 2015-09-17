249fb88ad97db748772a179e36b94534230a3166
bashell <nju.liuc@gmail.com>
2015-09-17 16:56:45 +0800

    first commit

---
+++ sorting/selectionSort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void selectionSort(int a[], int len)
+{
+    int i, j;
+    int min_index;
+    int temp;
+    
+    // 选择排序每轮都将本轮处理的最小元素放到最终的位置.
+    for(i = 0; i < len - 1; i++)
+    {
+        min_index = i;
+        for(j = i + 1; j < len; j++)
+            if(a[j] < a[min_index])
+                min_index = j;
+
+        if(min_index != i)  // 发现新min_index时, 进行swap操作.
+        {
+            temp = a[i];
+            a[i] = a[min_index];
+            a[min_index] = temp;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int test[10] = {1, 3, 2, 9, 10, 8, 4, 5, 7, 6};
+    selectionSort(test, 10);
+    for(i = 0; i < 10; i++)
+        printf("%d ", test[i]);
+    printf("\n");
+    
+    exit(EXIT_SUCCESS);
+}
---
+++ sorting/shellSort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void shellSort(int a[], int len)
+{
+    int i, j, increment;    // increment为增量步长
+    int temp;
+
+    // Using Shell's increments(not very efficient)
+    for(increment = len / 2; increment > 0; increment /= 2)
+        for(i = increment; i < len; i++)
+        {
+            temp = a[i];
+            for(j = i; j >= increment; j -= increment)
+                if(temp < a[j - increment])
+                    a[j] = a[j - increment];
+                else
+                    break;  // 用于跳过j -= increment的操作
+            a[j] = temp;
+        }
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int test[10] = {1, 3, 2, 9, 10, 8, 4, 5, 6, 7};
+    shellSort(test, 10);
+    for(i = 0; i < 10; i++)
+        printf("%d ", test[i]);
+    printf("\n");
+
+    exit(EXIT_SUCCESS);
+}
