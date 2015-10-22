bf6d5efae150b54ed8c3e74132410ed0a89dfed5
bashell <nju.liuc@gmail.com>
2015-10-22 21:55:34 +0800

    update

--- sorting/bubbleSort.c
+++ sorting/bubbleSort.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void bubbleSort(int a[], int len)
-{
+void bubbleSort(int a[], int len) {
     int i, j;
     int temp;
-
     // 每轮都将本轮处理的最大元素放到最终的位置.
-    for(i = 0; i < len - 1; i++)
-        for(j = 0; j < len - 1 - i; j++)
-            if(a[j + 1] < a[j])
-            {
+    for(i = 0; i < len - 1; i++) {
+        for(j = 0; j < len - 1 - i; j++) {
+            if(a[j + 1] < a[j]) {
                 temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
             }
+        }
+    }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int i;
     int test[10] = {1, 3, 2, 9, 10, 8, 4, 5, 6, 7};
     bubbleSort(test, 10);
     for(i = 0; i < 10; i++)
         printf("%d ", test[i]);
     printf("\n");
-
-    exit(EXIT_SUCCESS);
+    return 0;
 }
--- sorting/insertSorting.c
+++ sorting/insertSorting.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void insertSort(int a[], int len)
-{
+void insertSort(int a[], int len) {
     int i, j;
     int temp;
 
-    for(i = 1; i < len; i++)
-    {
+    for(i = 1; i < len; i++) {
         temp = a[i];
         for(j = i - 1; j >= 0 && a[j] > temp; j--)
             a[j + 1] = a[j];    // 所有大于temp的元素都后移1位
@@ -15,14 +12,12 @@ void insertSort(int a[], int len)
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int i;
     int test[10] = {1, 3, 2, 9, 10, 8, 4, 5, 7, 6};
     insertSort(test, 10);
     for(i = 0; i < 10; i++)
         printf("%d ", test[i]);
     printf("\n");
-
-    exit(EXIT_SUCCESS);
+    return 0;
 }
