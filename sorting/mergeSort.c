ecc0d7d03e8740f5cc6e6c7360b70741b901618c
bashell <nju.liuc@gmail.com>
2015-11-30 22:06:54 +0800

    update mergeSort

--- sorting/mergeSort.c
+++ sorting/mergeSort.c
@@ -1,57 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mergeSort(int a[], int len) {
-    int *tempArray;
+#define N 10
 
-    tempArray = malloc(len * sizeof(int));
-    if(tempArray == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    mSort(a, tempArray, 0, len - 1);
+void merge(int a[], int tempArray[], int left, int mid, int right) {
+    int i = left, j = mid, k = left;
+    int numElements = right - left + 1;    // the size of tempArray
+
+    while(i <= mid-1 && j <= right)
+        if(a[i] <= a[j])
+            tempArray[k++] = a[i++];
+        else
+            tempArray[k++] = a[j++];
+    while(i <= mid-1)    // copy the remaining elements of the left side
+        tempArray[k++] = a[i++];
+    while(j <= right)    // copy the remaining elements of the right side
+        tempArray[k++] = a[j++];
+    for(i = 0; i < numElements; ++i, --right)
+        a[right] = tempArray[right];
 }
 
-void mSort(int a[], int tempArray[], int left, int right) {
+void mergeSort(int a[], int tempArray[], int left, int right) {
     int center;
-
     if(left < right) {
         center = (left + right) / 2;
-        mSort(a, tempArray, left, center);
-        mSort(a, tempArray, center + 1, right);
+        mergeSort(a, tempArray, left, center);    // recursive call
+        mergeSort(a, tempArray, center + 1, right);
         merge(a, tempArray, left, center + 1, right);
     }
 }
 
-void merge(int a[], int tempArray[], int lpos, int rpos, int rightEnd) {    // lpos: 左半部分起始点  rpos: 右半部分起始点
-    int i, leftEnd, numElements, tempPos;
-
-    leftEnd = rpos - 1;    // 左半部分终点
-    tempPos = lpos;
-    numElements = rightEnd - lpos + 1;    // 元素总数
-
-    while(lpos <= leftEnd && rpos <= rightEnd)    // 将两个部分值小的元素加入到tempArray中
-        if(a[lpos] <= a[rpos])
-            tempArray[tempPos++] = a[lpos++];
-        else
-            tempArray[tempPos++] = a[rpos++];
-
-    while(lpos <= leftEnd)  // 拷贝左半部分剩余元素
-        tempArray[tempPos++] = a[lpos++];
-    while(rpos <= rightEnd) // 拷贝右半部分剩余元素
-        tempArray[tempPos++] = a[rpos++];
-
-    // 将tempArray[]写入到a[]中
-    for(i = 0; i < numElements; i++, rightEnd--)
-        a[rightEnd] = tempArray[rightEnd];
-}
-
 int main(int argc, char *argv[]) {
     int i;
-    int test[10] = {1, 3, 2, 9, 10, 8, 4, 5, 6, 7};
-    mergeSort(test, 10);
-    for(i = 0; i < 10; i++)
+    int test[N] = {1, 3, 2, 9, 10, 8, 4, 5, 6, 7};
+    int *tempArray = malloc(N * sizeof(int));
+    if(tempArray == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    mergeSort(test, tempArray, 0, N - 1);
+    for(i = 0; i < N; ++i)
         printf("%d ", test[i]);
     printf("\n");
+    free(tempArray);
     return 0;
 }
