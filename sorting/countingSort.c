f5ef1d2687b51cada2d3826b90c362b699c1c70c
bashell <nju.liuc@gmail.com>
2016-01-13 10:17:01 +0800

    modify

--- sorting/countingSort.c
+++ sorting/countingSort.c
@@ -6,8 +6,8 @@
  */
 void countingSort(int arr[], int n) {
     if(n <= 0)  return;
-    int i, max = -100000;
-    for(i = 0; i < n; ++i)
+    int i, max = arr[0];
+    for(i = 1; i < n; ++i)
         if(max < arr[i])
             max = arr[i];
     int *count = (int *)calloc(max+1, sizeof(int));
