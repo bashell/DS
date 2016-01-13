52128841c990f18cbdd09f2b0cd297f5d9340a55
bashell <nju.liuc@gmail.com>
2016-01-13 09:27:35 +0800

    add some comments

--- sorting/countingSort.c
+++ sorting/countingSort.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * T(N) = O(N + K)
+ */
 void countingSort(int arr[], int n) {
     if(n <= 0)  return;
     int i, max = -100000;
@@ -19,13 +22,17 @@ void countingSort(int arr[], int n) {
     }
     for(i = 0; i < n; ++i)  // 统计每个元素出现的总次数, 循环结束后count[i]等于值为i的元素个数
         ++count[arr[i]];
-    for(i = 1; i <= max; ++i)  // 累加计数, 循环结束后count[i]代表有多少个元素是小于等于i的
+    for(i = 1; i <= max; ++i)  // 累加计数, 循环结束后count[i]代表有多少个元素的值是小于等于i的
         count[i] += count[i-1];
+    /*
+     * 对原数组中的每个元素arr[i], 在count数组中查出有几个元素是比arr[i]小的, 用count[arr[i]]表示
+     * 若有m个元素比arr[i]小, 那么应该将arr[i]放在sorted数组的第m个位置上: sorted[m-1]
+     */
     for(i = n-1; i >= 0; --i) {
-        sorted[count[arr[i]]-1] = arr[i];  // 把每个元素arr[i]放在sorted数组中的正确位置上
+        sorted[count[arr[i]]-1] = arr[i];  // 按这种方法放置, 相同元素的相对位置不变, 因此算法是稳定的
         --count[arr[i]];  // 放完一个arr[i], 小于等于arr[i]的总个数count[arr[i]]当然要减1
     }
-    for(i = 0; i < n; ++i)  // 回写目标数组, 使之有序
+    for(i = 0; i < n; ++i)  // 回写, 结束后arr有序
         arr[i] = sorted[i];
     free(count);
     free(sorted);
