db04010e101b37cc688af9474812df7c08245ed7
bashell <nju.liuc@gmail.com>
2016-09-01 20:16:37 +0800

    iterative quicksort

--- sorting/quickSort.c
+++ sorting/quickSort.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 100
+#define N 20
 
-void insertSort(int *a, int len) {
+void insertionSort(int *a, int len) {
     int i, j, temp;
     for(i = 1; i < len; ++i) {
         temp = a[i];
@@ -20,9 +20,7 @@ void Swap(int *left, int *right) {
     *right = temp;
 }
 
-/*
- * "Hoare" partition
- */
+// "Hoare" partition
 int partition(int *a, int low, int high) {
     int i = low, j = high + 1, pivot = a[low];  // pivot: a[low] 
     while(1) {
@@ -38,25 +36,50 @@ int partition(int *a, int low, int high) {
     return j;
 }
 
-void quickSort(int *a, int low, int high) {
+// Recursive
+void quickSortRecursive(int *a, int low, int high) {
     int len = high - low + 1;
     if(low >= high)
         return ;
     if(len <= 10) {
-        insertSort(a + low, len);
+        insertionSort(a + low, len);
     } else {
         int p = partition(a, low, high);
-        quickSort(a, low, p - 1);
-        quickSort(a, p + 1, high);
+        quickSortRecursive(a, low, p - 1);
+        quickSortRecursive(a, p + 1, high);
     }
 }
+
+// Iterative
+void quickSortIterative(int *a, int low, int high) {
+    int pivot, top = -1, stack[high-low+1];
+    stack[++top] = low;
+    stack[++top] = high;
+    while(top >= 0) {
+        high = stack[top--];
+        low = stack[top--];
+        pivot = partition(a, low, high);
+        if(pivot-1 > low) {
+            stack[++top] = low;
+            stack[++top] = pivot-1;
+        }
+        if(pivot+1 < high) {
+            stack[++top] = pivot+1;
+            stack[++top] = high;
+        }
+    }
+}
+
 
 int main(int argc, char *argv[]) {
     int i, test[N];
     srand((unsigned)time(NULL));
     for(i = 0; i < N; ++i)
       test[i] = rand() % 100;
-    quickSort(test, 0, N - 1);
+
+    //quickSortRecursive(test, 0, N - 1);
+    quickSortIterative(test, 0, N - 1);
+
     for(i = 0; i < N; ++i)
         printf("%d ", test[i]);
     printf("\n");
